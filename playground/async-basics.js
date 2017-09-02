d5e04e9ec8d5d0ca2f7db24451ea80fcf8e1cd73
Filipe Valente Gomes <Filipe@Filipes-iMac.fios-router.home>
2017-09-02 17:14:40 -0400

    weather app started

---
+++ playground/async-basics.js
@@ -0,0 +1,11 @@
+// console.log('Starting app');
+//
+// setTimeout(() => {
+//   console.log("inside a callback")
+// },2000);
+//
+// setTimeout(() => {
+//   console.log("inside another callback")
+// },0);
+//
+// console.log('Finishing up');
